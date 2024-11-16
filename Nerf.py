c598c6241648506bd7ec1882603ccec158f4932d
le tri <triledacminh@gmail.com>
2024-11-17 01:46:51 +0700

    pose to rays

--- Nerf.py
+++ Nerf.py
@@ -15,11 +15,12 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class NERF(nn.Module):
-    def __init__(
-        self,
-    ):
+    """Nerf model implementation"""
+
+    def __init__(self):
+        """The architecture from the source code of the paper."""
         super().__init__()
-        # config
+        # Config.
         self.n_l_net = 8
         self.d_filter = 256
         self.d_input = 3
@@ -53,9 +54,26 @@ class NERF(nn.Module):
 
         self.rgb_unit = nn.Linear(self.d_u_filter, 3)
 
-    def forward(  # TODO: whole batch or single rays ? that is whole batch.
-        self, x, view_dir
-    ):
+    # NOTE : whole batch or single rays ? that is whole batch.
+    def forward(self, x, view_dir):
+        """Forward path.
+
+        Paprameters
+        ___________
+
+        x : Tensor
+            The coordiantes of the samples on rays, expecting shape (batch_size, 3).
+        view_dir : Tensor
+            The view direction vector of the sample on rays, expecting shape (batch_size, 3).
+
+        Returns
+        _______
+
+        Tensor
+            a tensor has rgb, voxel value, expecting shape (batch_size, 3 + 1).
+
+        """
+
         x_ = x
         for i, layer in enumerate(self.low_lays):
             if i in self.skip:
@@ -76,8 +94,26 @@ class NERF(nn.Module):
 
 
 class BlenderDataset(Dataset):
+    """Source: http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
+
+    Only load train set from lego dataset.
+
+    Attributes
+    __________
+
+    poses : Tensor
+        Poes matrix of images, expecting shape (n_image, 4, 4)
+    imgs : Tensor
+        Images data that has been scaled, expecting shape (n_images, height, width, 3),
+    H : scalar
+        Height of the images.
+    W : scalar
+        Width of the images.
+    focal : scalar
+        Focal length of the camera.
+    """
+
     def __init__(self):
-        # Contain poses (tensor), images (tensor C x H x W), height, width, camera focal length from train set.
         self.basedir = os.path.join(ROOT_DIR, "data", "nerf_synthetic", "lego")
         meta = None
         with open(os.path.join(self.basedir, "transforms_train.json")) as fl:
@@ -89,8 +125,13 @@ class BlenderDataset(Dataset):
             img = imageio.imread(fn)
             imgs.append(img)
             poses.append(np.array(frame["transform_matrix"]))
+            # DEBUG:  purpose
+            break
+            # DEBUG: purpose
+
         self.poses = tc.from_numpy(np.array(poses).astype(np.float32))
         self.imgs = (np.array(imgs) / 255.0).astype(np.float32)
+        # white background
         self.imgs = (255.0 * (1 - self.imgs[..., -1:])) + (
             self.imgs[..., :-1] * self.imgs[..., -1:]
         )
@@ -103,8 +144,56 @@ class BlenderDataset(Dataset):
         return self.imgs.shape[0]
 
     def __getitem__(self, index):
-        return self.imgs[index]
+        # TODO : optimze.
+        """
+
+        Involved in inference phase.
+        Returns
+        _______
+
+        rays_o : Tensor
+            rays origin.
+        rays_d : Tensor
+            rays direction, expecting shape (H, W, 3)
+
+
+        """
+        img = self.imgs[index]
+        pose = self.poses[index]
+
+        x_grid, y_grid = tc.meshgrid(
+            tc.arange(self.W, dtype=tc.float32),
+            tc.arange(self.H, dtype=tc.float32),
+            indexing="xy",
+        )
+        _x_grid, _y_grid = tc.meshgrid(
+            tc.arange(self.W, dtype=tc.float32),
+            tc.arange(self.H, dtype=tc.float32),
+            indexing="ij",
+        )
+        # TEST:
+        # _x_grid = _x_grid.transpose(-1, -2)
+        # _y_grid = _y_grid.transpose(-1, -2)
+        # print((x_grid == _x_grid).all())
+        # print((y_grid == _y_grid).all())
+
+        # NOTE : Why divide to focal length ?
+        # NOTE : Should is rays_dir (H, W, 3) or (W, H, 3). In there (W, H, 3)
+        direction = tc.stack(
+            [
+                (x_grid - 0.5 * self.W) / self.focal,
+                (0.5 * self.H - y_grid) / self.focal,
+                -tc.ones_like(x_grid),
+            ],
+            dim=-1,
+        )
+        # direction shape (W, H, 3)
+        rays_d = tc.sum(pose[:3, :3] * direction[..., None, :], dim=-1)
+        rays_o = pose[:-1, -1].expand(rays_d.shape)  # NOTE : redundant.
+
+        return rays_o, rays_d
 
 
 if __name__ == "__main__":
-    k = BlenderDataset()
+    data = BlenderDataset()
+    data[0]
--- tempt.py
+++ tempt.py
@@ -1,13 +1,21 @@
+from numpy._core.multiarray import dtype
 import torch as tc
 import numpy as np
 
 
-def test():
-    return tc.rand((1, 2)), tc.rand((2, 3))
+def split():
+    print("-----------")
 
 
 if __name__ == "__main__":
-    t = tc.rand(10, 2, 100, 100, 3)
-    a = tc.rand(100, 100, 3, 1)
-    d = tc.cat((t, a), dim=1)
-    print(d.shape)
+    # a, b = tc.meshgrid(
+    #     tc.arange(10, dtype=tc.float32), tc.arange(5, dtype=tc.float32), indexing="ij"
+    # )
+    # a = a.transpose(1, 0)
+    # b = b.transpose(1, 0)
+    # print(a.dtype)
+    # split()
+    # print(b.dtype)
+
+    a = tc.tensor([[1, 2]])
+    print(a.expand(5, 5))
